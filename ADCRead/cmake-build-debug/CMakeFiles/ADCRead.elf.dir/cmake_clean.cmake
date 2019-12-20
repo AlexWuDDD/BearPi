a76c90281592383ec53c9c835d4b5a9098616731
AlexWuDDD <wkjworking@outlook.com>
2019-12-20 14:39:13 +0800

    2019-12-20

---
+++ ADCRead/cmake-build-debug/CMakeFiles/ADCRead.elf.dir/cmake_clean.cmake
@@ -0,0 +1,43 @@
+file(REMOVE_RECURSE
+  "ADCRead.elf"
+  "ADCRead.elf.pdb"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_adc_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/adc.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/dac.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/main.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/stm32l4xx_hal_msp.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/stm32l4xx_it.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/sys.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/system_stm32l4xx.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/Src/usart.c.obj"
+  "CMakeFiles/ADCRead.elf.dir/startup/startup_stm32l431xx.s.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/ADCRead.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DACOutput/cmake-build-debug/CMakeFiles/DACOutput.elf.dir/cmake_clean.cmake
@@ -0,0 +1,40 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dac_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_exti.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_gpio.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_uart_ex.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/dac.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/gpio.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/main.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/stm32l4xx_hal_msp.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/stm32l4xx_it.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/sys.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/system_stm32l4xx.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/Src/usart.c.obj"
+  "CMakeFiles/DACOutput.elf.dir/startup/startup_stm32l431xx.s.obj"
+  "DACOutput.elf"
+  "DACOutput.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/DACOutput.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
